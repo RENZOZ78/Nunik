3fbda19548e1a15e3dc805eb3c49172467d3152a
RENZOZ78 <didierbap@gmail.com>
2021-01-12 00:47:47 +0100

    SAUVEGARDE INFOS DANS LA BDD

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,20 +4,49 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
 {
+    private $entityManager;
+
+    //METHOD2
+    public function __construct(EntityManagerInterface $entityManager) {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/inscription", name="register")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
 
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() &&$form->isValid() ){
+            $user  = $form->getData();
+
+           // dd($user); //PERMET DE VERIFIER SI L'ENTREE DES DONNES FONICTIONNENT
+
+            //ENREGISTRER LES INFOS DANS LA BDD GRACE A DOCTRINE
+                //METHODE1
+//                $doctrine = $this->getDoctrine()->getManager();
+                //METHODE2 CONSTRUCTOR
+//                $doctrine->persist($user);
+//                $doctrine->flush();
+                $this->entityManager->persist($user);
+                $this->entityManager->flush();
+
+
+
+        }
 
         return $this->render('register/index.html.twig',[
         'form' => $form->createView()
--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -34,19 +35,28 @@ class RegisterType extends AbstractType
                     'placeholder'=> 'Veuillez renseigner votre email'                ]
             ])
 //        ->add('roles')
-            ->add('password', PasswordType::class,[
-                'label' => 'Mot de passe',
-                'attr' => [
-                    'placeholder'=> 'Veuillez rensigner votre mot de passe'
-                ]
+//            ->add('password', PasswordType::class,[
+//                'label' => 'Mot de passe',
+//                'attr' => [
+//                    'placeholder'=> 'Veuillez rensigner votre mot de passe'
+//                ]
+//            ])
+//            ->add('password_confirm', PasswordType::class, [
+//                'label' => 'Confirmation mot de passe',
+//                'mapped' => false,
+//                'attr' => [
+//                    'placeholder' => 'Confirmez votre mot de passe',
+//                ]
+//    ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Lemot de passe et la confirmation doivent être identique.',
+                'label' => 'Votre mot de passe',
+                'required' => true,
+                'first_options' =>['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmez votre mot de passe']
+
             ])
-            ->add('password_confirm', PasswordType::class, [
-                'label' => 'Confirmation mot de passe',
-                'mapped' => false,
-                'attr' => [
-                    'placeholder' => 'Confirmez votre mot de passe',
-                ]
-    ])
             ->add('submit', SubmitType::class, [
                 'label' => "S'inscrire"
             ])
