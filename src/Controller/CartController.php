88e4bae71f9187cd13b6a9d4d938938ad53a07a1
RENZOZ78 <didierbap@gmail.com>
2021-01-26 01:07:01 +0100

    CREATION VUE RECAP AVANT ENTREE TUNNEL ACHAT

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,21 +3,44 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager )
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/mon-panier", name="cart")
      */
     public function index(Cart $cart): Response
     {
-        dd($cart->get());
+        $cartComplete = [];
+
+        foreach ($cart->get() as $id => $quantity ) {
+            $cartComplete[] = [
+                'product' => $this->entityManager->getRepository(Product::class)->findOneById($id),
+                'quantity' => $quantity
+            ];
+        }
+//        dd($cartComplete);
+
+//        dd($cart->get());
+
         return $this->render('cart/index.html.twig', [
-//            'controller_name' => 'CartController',
+            'cart' => $cartComplete
         ]);
+//            'controller_name' => 'CartController',
+//        'cart' => $cart->get()
+
     }
 
     /**
